10471fd36a99c72048ad62fc791574b6fa7fd917
shane <114774143+sdang10@users.noreply.github.com>
2023-11-21 14:50:08 -0800

    Update schema_setup.sql

--- OSM-SDOT/schema_setup.sql
+++ OSM-SDOT/schema_setup.sql
@@ -12,57 +12,66 @@ $$ LANGUAGE SQL IMMUTABLE STRICT;
 
 
 
-CREATE TABLE automated.users AS
-	ID INT8 PRIMARY KEY;
-	name TEXT;
-	email TEXT;
-	password TEXT;
+CREATE TABLE automated.users (
+	ID INT8 PRIMARY KEY,
+	username TEXT,
+	email TEXT,
+	password TEXT
+);
 
 
 
-CREATE TABLE automated.files AS
-	ID INT8 PRIMARY KEY;
-	name TEXT;
-	upload_dtm TIMESTAMP WITH TIME ZONE;
-	description TEXT;
-	FOREIGN KEY(user_ID) REFERENCES users(ID);
+CREATE TABLE automated.files (
+	ID INT8 PRIMARY KEY,
+	FOREIGN KEY(ID) REFERENCES automated.users(ID),
+	name TEXT,
+	upload_dtm TIMESTAMP WITH TIME ZONE,
+	description TEXT
+);
 
 
 
-CREATE TABLE automated.conflations AS
-	ID INT8 PRIMARY KEY;
-	FOREIGN KEY(base_file_ID) REFERENCES files(ID);
-	FOREIGN KEY(compared_file_ID) REFERENCES files(ID);
-	process_dtm TIMESTAMP WITH TIME ZONE;
+CREATE TABLE automated.conflations (
+	ID INT8 PRIMARY KEY,
+	base_file_ID INT8,
+	FOREIGN KEY(base_file_ID) REFERENCES automated.files(ID),
+	compared_file_ID INT8,
+	FOREIGN KEY(compared_file_ID) REFERENCES automated.files(ID),
+	process_dtm TIMESTAMP WITH TIME ZONE
+);
 
 
 
-CREATE TABLE automated.results AS
-	OSM_ID INT8;
-	start_end_seg TEXT;
-	SDOT_ID INT4;
-	highway TEXT;
-	OSM_surface TEXT;
-	SDOT_surface TEXT;
-	width NUMERIC;
-	tags hstore;
-	conflation_score float8;
-	original_way TEXT;
-	way geometry;
-	FOREIGN KEY(conflation_ID) REFERENCES conflations(ID);
-	FOREIGN KEY(user_ID) REFERENCES users(ID);
-	status INT8;
-	action_dtm TIMESTAMP WITH TIME ZONE;
+CREATE TABLE automated.results (
+	OSM_ID INT8,
+	start_end_seg TEXT,
+	SDOT_ID INT4,
+	highway TEXT,
+	OSM_surface TEXT,
+	SDOT_surface TEXT,
+	width NUMERIC,
+	tags hstore,
+	conflation_score float8,
+	original_way TEXT,
+	way geometry,
+	conflation_ID INT8,
+	FOREIGN KEY(conflation_ID) REFERENCES automated.conflations(ID),
+	user_ID INT8,
+	FOREIGN KEY(user_ID) REFERENCES automated.users(ID),
+	status INT8,
+	action_dtm TIMESTAMP WITH TIME ZONE
+);
 
 
 
-CREATE TABLE automated.metrics AS
-	FOREIGN KEY(conflation_ID) REFERENCES conflations(ID);
-	FOREIGN KEY(file_ID) REFERENCES files(ID);
-	FOREIGN KEY(file_name) REFERENCES files(name);
-	segment_ID INT8;
-	geom geometry;
-	percent_score float8;
-	conflated_geoms geometry;
+CREATE TABLE automated.metrics (
+	conflation_ID INT8,
+	FOREIGN KEY(conflation_ID) REFERENCES automated.conflations(ID),
+	file_ID INT8,
+	FOREIGN KEY(file_ID) REFERENCES automated.files(ID),
+	segment_ID INT8,
+	geom geometry,
+	percent_score float8,
+	conflated_geoms geometry
+);
 	
-
